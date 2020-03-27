82a488741ea457319d9e2f4c34d639b2d56a3849
ArturLinnik <arturlin2018@gmail.com>
2020-03-27 08:25:42 +0100

    Print Action Space and State Space

--- RL_gym/test.py
+++ RL_gym/test.py
@@ -5,6 +5,15 @@ env = gym.make("Taxi-v3").env
 
 env.render()
 
+env.reset() # Reset the environment
+
+env.render ()
+
+print("Action Space:", env.action_space)
+
+print("State pace:", env.observation_space)
+
+
 
 
 
