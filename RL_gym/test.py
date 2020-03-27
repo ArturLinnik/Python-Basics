242e1e8795be04a480caaebb7806852b3687afb0
ArturLinnik <arturlin2018@gmail.com>
2020-03-27 08:33:58 +0100

    Displays in terminal a brute force example

--- RL_gym/test.py
+++ RL_gym/test.py
@@ -1,24 +1,10 @@
 
+import os
+# from IPython.display import clear_output ### IPython doesn't work for me
+from time import sleep
 import gym
 
 env = gym.make("Taxi-v3").env
-
-# env.render()
-
-# env.reset() # Reset the environment
-
-# env.render ()
-
-# print("Action Space:", env.action_space)
-
-# print("State pace:", env.observation_space)
-
-# state = env.encode(4,2,2,1) # (taxi row, taxi column, passenger index, destination index)
-
-# print("State:", state)
-
-# env.s = state
-# env.render()
 
 env.s = 449
 env.render()
@@ -29,6 +15,53 @@ for i,n in env.P[449].items():
 # {action: [(probability, nextstate, reward, done)]}
 
 # action --> south, north, east, west, pickup, dropoff
+
+epochs = 0
+penalties, reward = 0,0
+
+frames = [] # for animation
+
+done = False
+
+while not done:
+    action = env.action_space.sample() # Randomly executes an action
+    state, reward, done, info = env.step(action)
+
+    if reward == -10:
+        penalties += 1
+
+
+    # Put each rendered frame into dict for animation
+
+    frames.append(
+        {
+        'frame': env.render(mode='ansi'),
+        'state': state,
+        'action': action,
+        'reward': reward
+        }
+    )
+
+    epochs += 1
+
+print("Timesteps taken", epochs)
+print("Penalties taken", penalties)
+
+def clear():
+    os.system('clear')
+
+def print_frames(frames):
+    for i, frame in enumerate(frames):
+        clear()
+        # clear_output(wait=True) ### IPython doesn't work for me
+        print(frame.get('frame'))
+        print(f"Timestep: {i + 1}")
+        print(f"State: {frame['state']}")
+        print(f"Action: {frame['action']}")
+        print(f"Reward: {frame['reward']}")
+        sleep(.1)
+
+print_frames(frames)
 
 
 
