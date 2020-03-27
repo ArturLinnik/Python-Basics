dbcb151a4330f4b5dc782d3e54c3bbe49fc33ec3
ArturLinnik <arturlin2018@gmail.com>
2020-03-27 08:30:45 +0100

    Sets a state and prints the dictionary with the info

--- RL_gym/test.py
+++ RL_gym/test.py
@@ -3,22 +3,32 @@ import gym
 
 env = gym.make("Taxi-v3").env
 
+# env.render()
+
+# env.reset() # Reset the environment
+
+# env.render ()
+
+# print("Action Space:", env.action_space)
+
+# print("State pace:", env.observation_space)
+
+# state = env.encode(4,2,2,1) # (taxi row, taxi column, passenger index, destination index)
+
+# print("State:", state)
+
+# env.s = state
+# env.render()
+
+env.s = 449
 env.render()
 
-env.reset() # Reset the environment
+for i,n in env.P[449].items():
+    print(i,n)
 
-env.render ()
+# {action: [(probability, nextstate, reward, done)]}
 
-print("Action Space:", env.action_space)
-
-print("State pace:", env.observation_space)
-
-state = env.encode(4,2,2,1) # (taxi row, taxi column, passenger index, destination index)
-
-print("State:", state)
-
-env.s = state
-env.render()
+# action --> south, north, east, west, pickup, dropoff
 
 
 
