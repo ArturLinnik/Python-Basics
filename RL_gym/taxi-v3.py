2ce733c770e92c9c58c7c9b40b437a822d89e278
ArturLinnik <arturlin2018@gmail.com>
2020-03-27 19:45:28 +0100

    Evaluating the trained agent

--- RL_gym/taxi-v3.py
+++ RL_gym/taxi-v3.py
@@ -13,41 +13,39 @@ env.render()
 for i,n in env.P[449].items():
     print(i,n)
 
-### Training the agent ###
+### Training the Agent ###
 
-q_table = numpy.zeros([env.observation_space.n, env.action_space.n]) # .n represents the size 
+q_table = numpy.zeros([env.observation_space.n, env.action_space.n])
 
 # Hyperparameters
-
 alpha = 0.1
 gamma = 0.6
 epsilon = 0.1
 
 # For plotting metrics
-
 all_timesteps = []
 all_penalties = []
 
 def clear():
     os.system('clear')
 
-for i in range(1,100001):
+for i in range(1, 100001):
     state = env.reset()
 
-    timesteps, penalties, reward = 0, 0, 0
+    timesteps, penalties, reward, = 0, 0, 0
     done = False
-
+    
     while not done:
-        if random.uniform(0,1) < epsilon:
+        if random.uniform(0, 1) < epsilon:
             action = env.action_space.sample() # Explore action space
         else:
             action = numpy.argmax(q_table[state]) # Exploit learned values
 
-        next_state, reward, done, info = env.step(action)
-
+        next_state, reward, done, info = env.step(action) 
+        
         old_value = q_table[state, action]
         next_max = numpy.max(q_table[next_state])
-
+        
         new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
         q_table[state, action] = new_value
 
@@ -56,17 +54,41 @@ for i in range(1,100001):
 
         state = next_state
         timesteps += 1
-
+        
     if i % 100 == 0:
         clear()
-        print("Episode:", i)
+        print(f"Episode: {i}")
 
-print("Training finished. \n")
-
+print("Training finished.\n")
 print(q_table[449])
 
+### Evaluating the agent after Q-learning ###
+
+total_timesteps, total_penalties = 0,0
+episodes = 100
+
+for _ in range(episodes):
+    state = env.reset()
+    timesteps, penalties, reward = 0, 0, 0
+
+    done = False
+    
+    while not done:
+        action = numpy.argmax(q_table[state])
+        state, reward, done, info = env.step(action)
+
+        if reward == -10:
+            penalties += 1
+
+        timesteps += 1
 
 
+    total_penalties += penalties
+    total_timesteps += timesteps
+
+print("Results after ", episodes, "episodes:")
+print("Average timesteps per episode:", total_timesteps/episodes)
+print("Average penalties per episode:", total_penalties/episodes)
 
 
 
