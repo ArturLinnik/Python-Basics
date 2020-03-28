6bba1d82e49a3ae37d0ba0ae0e1d5e11ee4d2a53
ArturLinnik <arturlin2018@gmail.com>
2020-03-28 20:51:19 +0100

    Jumping and boundaries

--- Pygame/Red_rectangle.py
+++ Pygame/Red_rectangle.py
@@ -9,20 +9,25 @@ pygame.display.set_caption("First Game")
 
 # We have to set the characters parameters
 
-x = 50
-y = 50
+x = 250
+y = 250
 
 width = 40
 height = 60
 
-vel = 5
+vel = 10
+
+# Jumping
+
+isJump = False
+jumpCount = 10
 
 # Programming in the window
 
 running = True
 
 while running:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
 
     # Enabling the quit button (X)
     for event in pygame.event.get():
@@ -33,17 +38,35 @@ while running:
     keys = pygame.key.get_pressed()
 
     ## IMPORTANT! -- The (0,0) is on the top-left of the screen, on the bottom-right is (500,500) (In our case)
-    if keys[pygame.K_LEFT]:
+    # The "and" are for the character not exiting the screen
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
         x += vel
 
-    if keys[pygame.K_UP]:
-        y -= vel
+    # When we jump we do not want to to be able to move up and down
+    if not isJump:
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
 
-    if keys[pygame.K_DOWN]:
-        y +=vel
+        if keys[pygame.K_DOWN] and y < 500 - height - vel:
+            y +=vel
+        
+        # Jumping key
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            sign = 1
+            if jumpCount < 0:
+                sign = -1
+            y -= (jumpCount**2) * 0.5 * sign
+            jumpCount -= 1
+
+        else:
+            isJump = False
+            jumpCount = 10
 
     # For the character to move without copying constantly its image
     window.fill((0,0,0))
